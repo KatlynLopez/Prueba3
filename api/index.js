1b4e707d6052161200eeb75b8b9a741e8ad98a47
Keylinda <alelorios0503@gmail.com>
2025-06-21 09:03:11 -0600

    nuevo

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ const port = 5000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-const login = require("./src/routes/login")
-const teacher = require("./src/routes/teacher.routes")
+const login = require("../src/routes/login")
+const teacher = require("../src/routes/teacher.routes")
 
 const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
 
