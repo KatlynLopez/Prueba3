033be720448bf5b9d3820fb54f31950e0749ccc2
Keylinda <alelorios0503@gmail.com>
2025-06-21 08:43:13 -0600

    nuevo

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = 5000
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const login = require("./src/routes/login")
+const teacher = require("./src/routes/teacher.routes")
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
+
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -17,3 +29,8 @@ app.get("/api", function(req, res) {
 
 app.use("/api/login", login);
 app.use("/api/teacher", teacher);
+
+ 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
